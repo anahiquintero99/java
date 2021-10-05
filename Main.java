5cb6c7b458c432bce496a3879dd513219d91b0f4
Anahi Quintero <anitaquintero99@gmail.com>
2021-10-04 23:25:43 -0500

    Variables Java

---
+++ Main.java
@@ -0,0 +1,7 @@
+import javax.swing.text.View;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world");
+  }// End of main
+}// End of FirstJavaProgram Class
---
+++ ejercicios/Variables.java
@@ -0,0 +1,12 @@
+package ejercicios;
+
+public class Variables {
+  public static void main(String[] args) {
+    // Declaro una variable
+    int salary = 800;
+    String emplyeename = "Anahi";
+
+    System.out.println(salary);
+    System.out.println(emplyeename);
+  }
+}
