ef16ec4e7fac2aa9318d92ba78762b3d16bacfd6
Anahi Quintero <anitaquintero99@gmail.com>
2021-10-04 23:59:56 -0500

    My first proyect in java

--- ejercicios/UpdatingVariables.java
+++ ejercicios/UpdatingVariables.java
@@ -2,14 +2,28 @@ package ejercicios;
 
 public class UpdatingVariables {
   public static void main(String[] args) {
+    // Salario
     int salary = 1000;
-    // bono $200
-    int bono = 200;
 
     // suma de bono y salario
+    int bono = 200;
     salary = salary + bono;
 
-    System.out.println(salary);
+    // Descontar pension $50
+    int pension = 50;
+    salary = salary - pension;
+
+    // 2 horas extra c/u $30
+    // Comoda $45
+    int hourExtra = 30;
+    int snaks = 45;
+
+    salary = (salary + (hourExtra * 2)) - snaks;
+
+    // nombre del empleado
+    String emplyeename = "Anahi Quintero";
+
+    System.out.println(emplyeename + " tiene un salario de $" + salary);
 
   }
 }
