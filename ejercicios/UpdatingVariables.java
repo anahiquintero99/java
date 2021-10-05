6418249fb723050aab7213318548bb220a412f5f
Anahi Quintero <anitaquintero99@gmail.com>
2021-10-04 23:41:26 -0500

    first commit

---
+++ ejercicios/UpdatingVariables.java
@@ -0,0 +1,15 @@
+package ejercicios;
+
+public class UpdatingVariables {
+  public static void main(String[] args) {
+    int salary = 1000;
+    // bono $200
+    int bono = 200;
+
+    // suma de bono y salario
+    salary = salary + bono;
+
+    System.out.println(salary);
+
+  }
+}
