0d6bbc27fdbdfa9b7f2dbfe22903961ead394f29
Anahi Quintero <anitaquintero99@gmail.com>
2021-10-06 00:09:54 -0500

    changes to exercise DataVariables

--- ejercicios/DataVariables.java
+++ ejercicios/DataVariables.java
@@ -1,11 +1,15 @@
 package ejercicios;
 
 public class DataVariables {
-  int salary = 1000; //int
-  //pension 3%
-  double pension = salary * 0.03; //double
-  double totalySalary = salary - pension;
 
-  System.out.println(totalySalary);
-  
+  public static void main(String[] args) {
+    int salary = 1000; // int
+    // pension 3%
+    double pension = salary * 0.03; // double
+    double totalySalary = salary - pension;
+
+    String emplyeename = "Anahi";
+
+    System.out.println("El salario de " + emplyeename + " es de: $" + totalySalary);
+  }
 }
