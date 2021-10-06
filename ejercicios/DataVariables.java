01ccf61d7e3ed67be6f078367f6c91b069619306
Anahi Quintero <anitaquintero99@gmail.com>
2021-10-05 23:50:21 -0500

    New exercise to java

---
+++ ejercicios/DataVariables.java
@@ -0,0 +1,11 @@
+package ejercicios;
+
+public class DataVariables {
+  int salary = 1000; //int
+  //pension 3%
+  double pension = salary * 0.03; //double
+  double totalySalary = salary - pension;
+
+  System.out.println(totalySalary);
+  
+}
