9918915879ae0a8f0c52ae1e477605acd7115d41
Anahi Quintero <anitaquintero99@gmail.com>
2021-10-18 19:49:34 -0500

    New exercise to class math

---
+++ ejercicios/MathematicOperation.java
@@ -0,0 +1,14 @@
+package ejercicios;
+
+public class MathematicOperation {
+  public static void main(String[] args) {
+    double x = 2.1;
+    double y = 3;
+
+    // Devuelve un entero hacia arriba
+    System.out.println(Math.ceil(x));
+
+    // Devuelve un entero hacia abajo
+    System.out.println(Math.floor(x));
+  }
+}
