e48cc2703a107836469d6134c7add8c0b77a64b5
Anahi Quintero <anitaquintero99@gmail.com>
2021-10-27 18:46:24 -0500

    code modifications

---
+++ ejercicios/Casting.java
@@ -0,0 +1,16 @@
+package ejercicios;
+
+public class Casting {
+  public static void main(String[] args) {
+
+    // En un año ubicar 30 perritos
+    // Cuantos perritos ubique al mes
+
+    double monthlyDogs = 30.0 / 12.0;
+    System.out.println(monthlyDogs);
+
+    // Estimación
+    int estimatedMonthlyDogs = (int) monthlyDogs;
+    System.out.println(estimatedMonthlyDogs);
+  }
+}
